f5265eca251b280042bf8eeedc1af3a5af13b796
root <root@yukika-virtual-machine.(none)>
2013-12-04 16:12:09 +0900

    0

--- lib/topology.rb
+++ lib/topology.rb
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 require 'forwardable'
 require 'link'
-require 'host'
 require 'observer'
 require 'trema-extensions/port'
 
@@ -15,66 +14,59 @@ class Topology
 
   def_delegator :@ports, :each_pair, :each_switch
   def_delegator :@links, :each, :each_link
-  def_delegator :@hosts, :each, :each_host
 
   def initialize(view)
     @ports = Hash.new { [].freeze }
     @links = []
-    @hosts = []
     add_observer view
   end
 
   def delete_switch(dpid)
     @ports[dpid].each do | each |
-      delete_port dpid, each
+      delete_port each
     end
     @ports.delete dpid
   end
 
-  def update_port(dpid, port)
+  def update_port(port)
     if port.down?
-      delete_port dpid, port
+      delete_port port
     elsif port.up?
-      add_port dpid, port
+      add_port port
     end
   end
 
-  def add_port(dpid, port)
-    @ports[dpid] += [port]
+  def add_port(port)
+    @ports[port.dpid] += [port]
   end
 
-  def delete_port(dpid, port)
-    @ports[dpid] -= [port]
-    delete_link_by dpid, port
+  def delete_port(port)
+    @ports[port.dpid] -= [port]
+    delete_link_by port
   end
 
   def add_link_by(dpid, packet_in)
     fail 'Not an LLDP packet!' unless packet_in.lldp?
-
-    link = Link.new(dpid, packet_in)
-    unless @links.include?(link)
-      @links << link
-      @links.sort!
-      changed
-      notify_observers self
+    begin
+      maybe_add_link Link.new(dpid, packet_in)
+    rescue
+      return
     end
-  end
-
-  def add_host_by(dpid, packet_in)
-    host = Host.new(dpid, packet_in)
-    unless @hosts.include?(host)
-      @hosts << host
-      @hosts.sort!
-      changed
-      notify_observers self
-    end
+    changed
+    notify_observers self
   end
 
   private
 
-  def delete_link_by(dpid, port)
-    @links.each do | each |
-      if each.has?(dpid, port.number)
+  def maybe_add_link(link)
+    fail 'The link already exists.' if @links.include?(link)
+    @links << link
+    @links.sort!
+  end
+
+  def delete_link_by(port)
+    @links.each do |each|
+      if each.has?(port.dpid, port.number)
         changed
         @links -= [each]
       end
--- lib/view/text.rb
+++ lib/view/text.rb
@@ -12,9 +12,6 @@ module View
       topology.each_link do | each |
         info each.to_s
       end
-      topology.each_host do | each |
-        info each.to_s
-      end
       info 'topology updated'
     end
   end
