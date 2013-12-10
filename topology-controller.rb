e373db0377df4278fd3f3763c172f21e3185f762
s-nakamr <s-nakamr@ist.osaka-u.ac.jp>
2013-12-10 15:59:19 +0900

    bug fixed

--- topology-controller.rb
+++ topology-controller.rb
@@ -42,7 +42,7 @@ class TopologyController < Controller
   end
 
   def packet_in(dpid, packet_in)
-    @topology.add_host dpid, packet_in unless packet_in.ipv4?
+    @topology.add_host dpid, packet_in if packet_in.ipv4?
     @topology.add_link_by dpid, packet_in
   end
 
