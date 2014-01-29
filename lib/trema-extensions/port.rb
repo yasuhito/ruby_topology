94526b5307e7b1e4b4eceb3facebfa8ad23b8b67
shoheifujii <fujii.shohei@ist.osaka-u.ac.jp>
2014-01-30 07:59:10 +0900

    fixing port.rb

---
+++ lib/trema-extensions/port.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+require 'trema-extensions/port/predicates'
+
+module Trema
+  #
+  # Inserts monkey-patches to Trema::Port
+  #
+  class Port
+    include TremaExtensions::Port
+  end
+end
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
